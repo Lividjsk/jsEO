77bc852abb096a4a1f5834777b2665e821f1400e
Javuer <jgg00045@red.ujaen.es>
2017-02-23 21:44:19 +0100

    Operador de mutación creado. Utilizamos la mutación por intercambio.

--- js/libs/jsEO/jsEORepresentationOrder/jsEOROOpMutation.js
+++ js/libs/jsEO/jsEORepresentationOrder/jsEOROOpMutation.js
@@ -24,16 +24,32 @@ var jsEOROOpMutation = new Class({
 
     },
     operate: function(_auxPop) {
-        jsEOUtils.debugln("Applying jsEOROOpBitFlip");
+        jsEOUtils.debugln("Applying jsEOROOpMutation");
         var toRet = new jsEOPopulation();
-        var tmpChr = _auxPop.getAt(0).getChromosome();
-        var newChr = [];
-        jsEOUtils.debugln("  Individual is " + tmpChr);
-        for (var i = 0; i < tmpChr.length; ++i) {
-            newChr.push( (Math.random() < this.genesRate) ? (Math.random()*(this.max-this.min)+this.min) : tmpChr[i]);
+        var newInd = new jsEOROIndividual();
+        var tam = _auxPop.getAt(0).length;
+        var number_mutation = Math.ceil(_auxPop.length() * tam * this.genesRate);
+        var chromosome = 0;
+        var gen_mutation = 0;
+        var gen_mutation_2 = 0;
+        var value = 0;
+        var newChr;
+        for (var i = 0; i < number_mutation; ++i) {
+            gen_mutation = Math.random() % ((_auxPop.length() * _auxPop.getAt(0).length) - 1);
+            chromosome = gen_mutation / (_auxPop.getAt(0).length);
+            gen_mutation_2 = gen_mutation % tam;
+            while (gen_mutation_2 == gen_mutation) {
+                gen_mutation_2 = Math.random()() % (tam - 1);
+            }
+            newChr = _auxPop.getAt(chromosome);
+            jsEOUtils.debugln("  Individual is " + newChr);
+            value = auxChr[gen_mutation];
+            newChr[gen_mutation] = newChr[gen_mutation_2];
+            newChr[gen_mutation_2] = value;
+            //Preguntar a Victor donde se evalua al individuo una vez mutado
         }
         jsEOUtils.debugln("  Final  " + newChr);
-        toRet.add(new jsEOROIndividual());
+        toRet.add(new jsEOFVIndividual());
         toRet.getAt(0).setChromosome(newChr);
         return toRet;
     }
